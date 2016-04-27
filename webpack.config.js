e9d25b2e74f35aaf025cbfd6bf00d73dc9210dbb
Chris Jaynes (sc8l) <chris.jaynes.sc8l@statefarm.com>
2016-04-27 14:37:15 -0500

    tweak externals

--- webpack.config.js
+++ webpack.config.js
@@ -6,47 +6,15 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "yolk.min.js",
-    library: "Yolk",
     libraryTarget: 'var',
-    umdNamedDefine: true
+    library: "Yolk"
   },
   externals: {
-    "rxjs": "Rx",
-    "rxjs/add/observable/combineLatest": {
-      commonjs: "rxjs/add/observable/combineLatest",
-      commonjs2: "rxjs/add/observable/combineLatest",
-      root: ["Rx", "Observable"]
-    },
-    "rxjs/add/observable/of": {
-      commonjs: "rxjs/add/observable/of",
-      commonjs2: "rxjs/add/observable/of",
-      root: ["Rx", "Observable"]
-    },
-    "rxjs/add/operator/map": {
-      commonjs: "rxjs/add/observable/of",
-      commonjs2: "rxjs/add/observable/of",
-      root: ["Rx", "Observable", "prototype"]
-    },
-    "rxjs/add/operator/mapTo": {
-      commonjs: "rxjs/add/observable/mapTo",
-      commonjs2: "rxjs/add/observable/mapTo",
-      root: ["Rx", "Observable", "prototype"]
-    },
-    "rxjs/add/operator/share":{
-      commonjs: "rxjs/add/observable/share",
-      commonjs2: "rxjs/add/observable/share",
-      root: ["Rx", "Observable", "prototype"]
-    },
-    "rxjs/add/operator/switchMap":{
-      commonjs: "rxjs/add/observable/switchMap",
-      commonjs2: "rxjs/add/observable/switchMap",
-      root: ["Rx", "Observable", "prototype"]
-    },
-    "rxjs/BehaviorSubject": "umd Rx",
-    "rxjs/Observable": "umd Rx",
-    "rxjs/Observer":"umd Rx",
-    "rxjs/Subject":"umd Rx",
-    "rxjs/Subscription":"umd Rx"
+    "rxjs/BehaviorSubject": "Rx",
+    "rxjs/Observable": "Rx",
+    "rxjs/Observer":"Rx",
+    "rxjs/Subject":"Rx",
+    "rxjs/Subscription":"Rx"
   },
   module: {
     loaders: [{
@@ -54,12 +22,12 @@ module.exports = {
       loader: "babel-loader",
       exclude: /(node_modules)/
     }]
-  },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    })
-  ]
+   }
+  // plugins: [
+  //   new webpack.optimize.UglifyJsPlugin({
+  //     compress: {
+  //       warnings: false
+  //     }
+  //   })
+  // ]
 };
